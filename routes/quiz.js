56abf04424fa54643999a92e0bf518a04c6146da
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-11-10 19:10:32 +0100

    Création d'une nouvelle route pour afficher une question aléatoire

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,6 +3,21 @@ const Quiz = require('../models/quiz');
 const quizCtrl = require('../controllers/quiz');
 const router = express.Router();
 
+router.get('/random', async (req, res) => {
+  try {
+    const randomQuiz = await Quiz.aggregate([{ $sample: { size: 1 } }]);
+
+    if (!randomQuiz || randomQuiz.length === 0) {
+      return res.status(404).json({ message: "Aucun qui trouvé."});
+    };
+
+    res.json(randomQuiz[0]);
+  } catch (error) {
+    console.error("Erreur durant la récupération du Quiz aléatoire :", error);
+    res.status(500).json({ error: "Impossiblie de récupérer un quiz"});
+  };
+});
+
 router.get('/', quizCtrl.findAllQuiz);
 router.post('/', quizCtrl.createQuiz);
 router.get('/:id', quizCtrl.findOneQuiz);
