657b6ff3bf407b1e576e49098179f95cabfa46b9
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2024-02-27 16:11:16 +0100

    Adding username in the localStorage when user login

--- controllers/user.js
+++ controllers/user.js
@@ -39,6 +39,7 @@ exports.login = (req, res, next) => {
           };
           res.status(200).json({
             userId: user._id,
+            username: user.username,
             token: jwt.sign(
               { userId: user._id },
               'RANDOM_TOKEN_SECRET',
