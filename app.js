6c0e87e45e8525fef623a6ddd84735f93ef75a55
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2024-05-10 16:01:26 +0200

    Modification du fichier app.js et server.js pour préparer la mise en prod

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 const express = require('express');
-const session = require('express-session');
 const mongoose = require('mongoose');
-const config = require('./config');
-const cors = require('cors');
 require('dotenv').config();
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('./config');
 const quizRoutes = require('./routes/quiz');
 const userRoutes = require('./routes/user');
 const app = express();
-const router = express.Router();
 
 
-mongoose.connect(config.mongoURI,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect(config.mongoURI)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(helmet());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-app.use(cors({
-  origin: 'http://localhost:3001',
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 
--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
+const normalizePort = val => parseInt(val, 10) || false;
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
@@ -19,15 +12,14 @@ const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
   }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  const bind = `port ${port}`;
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
+      console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
+      console.error(`${bind} is already in use`);
       process.exit(1);
       break;
     default:
@@ -35,13 +27,21 @@ const errorHandler = error => {
   }
 };
 
+// En attente de la validation https
+
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, 'ssl', 'privateKey.key')),
+//   cert: fs.readFileSync(path.join(__dirname, 'ssl', 'certificate.crt'))
+// };
+
+// const server = https.createServer(options, app);
+
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
 server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  const bind = `port ${port}`;
+  console.log(`Listening on ${bind}`);
 });
 
 server.listen(port);
