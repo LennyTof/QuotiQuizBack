b8d4540e9c949665b39ca72ae598842386fccd8e
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2024-05-31 13:42:23 +0200

    revert for now

--- server.js
+++ server.js
@@ -25,36 +25,6 @@ const errorHandler = error => {
   }
 };
 
-// Code pour récupérer l'adresse IP statique en production uniquement
-if (process.env.QUOTAGUARDSTATIC_URL) {
-  const options = {
-      hostname: process.env.QUOTAGUARDSTATIC_URL,
-      port: 80,
-      path: 'http://ip.quotaguard.com/',
-      headers: {
-          'Proxy-Authorization': `Basic ${Buffer.from(process.env.QUOTAGUARDSTATIC_USERNAME + ':' + process.env.QUOTAGUARDSTATIC_PASSWORD).toString('base64')}`,
-      }
-  };
-
-  http.get(options, (res) => {
-      let data = '';
-
-      // A chaque chunk de données reçu, on l'ajoute à la variable 'data'
-      res.on('data', (chunk) => {
-          data += chunk;
-      });
-
-      // Une fois que toutes les données ont été reçues, on les traite
-      res.on('end', () => {
-          const ipAddress = JSON.parse(data).ip;
-          console.log('Adresse IP statique récupérée:', ipAddress);
-          process.env.IP_ADDRESS = ipAddress; // Stockez l'adresse IP dans une variable d'environnement
-      });
-  }).on('error', (err) => {
-      console.error('Erreur lors de la récupération de l\'adresse IP statique:', err.message);
-  });
-}
-
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
